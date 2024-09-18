f2df33ae439deda064c2cfbf4ffca020c2b298d4
titly00 <baronshik007@gmim.com>
2024-09-18 18:49:33 +0600

    Ariet

--- main/models.py
+++ main/models.py
@@ -7,3 +7,25 @@ class CarBrand(models.Model):
     name = models.CharField(max_length=100)
     brands_car = models.CharField(max_length=101)
     info = models.TextField()
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=123)
+
+
+
+
+
+class Book(models.Model):
+    titel = models.CharField(max_length=103)
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=122)
+    Author = models.CharField(max_length=123)
+    info = models.TextField()
+
+
+
+
